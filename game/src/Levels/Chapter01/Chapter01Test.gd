59413f6d52590246182af33d820e740b0148913c
Carenalga <mapedorr@gmail.com>
2020-08-31 21:28:38 -0500

    Created animations using Aseprite Importer.
    Created collisions using the thing that creates the shape based on the alpha space of a PNG.
    Created Area2D collisions testing how to render the overlay that will show position of player behind things.

---
+++ game/src/Levels/Chapter01/Chapter01Test.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_OverlayArea_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.name == 'Player':
+		$Player/AnimationPlayer.play('idle')
+		$Player/Overlay.show()
+
+
+func _on_OverlayArea_body_shape_exited(body_id, body, body_shape, area_shape):
+	if body.name == 'Player':
+		$Player/AnimationPlayer.stop()
+		$Player/Overlay.hide()
