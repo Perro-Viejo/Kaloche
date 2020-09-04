71e82359a5fee790f9755bdd10282bfc1d9de8a1
QuietGeckoAudio <quietgecko@gmail.com>
2020-09-04 00:57:18 -0500

    added some music behaviour for audio triggers, added spot ambience to the basic trigger script, added enum with different audio types for manager

--- game/src/Audio/MusicManager.gd
+++ game/src/Audio/MusicManager.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var prev_mx = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +8,31 @@ func _ready():
 	AudioEvent.connect('mx_stop', self, 'stop_mx')
 
 
-func play_mx(music):
-	var sound_path := '%s' % [music]
+func _get_mx(_music) -> Node:
+	var sound_path := '%s' % [_music]
+	var node: Node = null
+	if has_node(sound_path):
+		node = get_node(sound_path)
+	return node
+
+func play_mx(music: String) -> void:
+	var audio: Node = _get_mx(music)
+	if audio.get('stream_paused'):
+		audio.stream_paused = false
+	else:
+		if not audio == prev_mx:
+			audio.play()
+			prev_mx = audio
+	if audio is AudioStreamPlayer or audio is AudioStreamPlayer2D:
+		if audio.is_connected('finished', self, '_on_finished'):
+			return
+		else:
+			audio.connect('finished', self, '_on_finished', [music])
+	else:
+		if audio.select_sound.is_connected('finished', self, '_on_finished'):
+			return
+		else:
+			audio.select_sound.connect('finished', self, '_on_finished', [music])
+
+func _on_finished(music: String):
+	AudioEvent.emit_signal('stream_finished', music)
