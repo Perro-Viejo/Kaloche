99a158d429d897165b9665b985a49f9ced0d87d4
Carenalga <mapedorr@gmail.com>
2020-09-03 20:23:44 -0500

    Updates in texture map and scene structure

--- game/src/Levels/Chapter01/Chapter01Test.gd
+++ game/src/Levels/Chapter01/Chapter01Test.gd
@@ -4,11 +4,13 @@ extends Node2D
 
 func _on_OverlayArea_body_shape_entered(body_id, body, body_shape, area_shape):
 	if body.name == 'Player':
-		$Player/AnimationPlayer.play('idle')
-		$Player/Overlay.show()
+		pass
+#		$Player/AnimationPlayer.play('idle')
+#		$Player/Overlay.show()
 
 
 func _on_OverlayArea_body_shape_exited(body_id, body, body_shape, area_shape):
 	if body.name == 'Player':
-		$Player/AnimationPlayer.stop()
-		$Player/Overlay.hide()
+		pass
+#		$Player/AnimationPlayer.stop()
+#		$Player/Overlay.hide()
