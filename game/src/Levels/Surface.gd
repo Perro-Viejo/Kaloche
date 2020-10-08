03f2fcf63c37d8684ea36f297df1ab8a8861abd1
Carenalga <mapedorr@gmail.com>
2020-10-08 13:09:19 -0500

    Changed a method name for better understanding

--- game/src/Levels/Surface.gd
+++ game/src/Levels/Surface.gd
@@ -4,11 +4,11 @@ export var fs_name := ''
 
 # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos de Godot ▒▒▒▒
 func _ready() -> void:
-	connect('body_entered', self, '_trigger_sfx')
+	connect('body_entered', self, '_assign_sfx')
 	connect('body_exited', self, '_stop_sfx')
 
 # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos privados ▒▒▒▒
-func _trigger_sfx(body: Node) -> void:
+func _assign_sfx(body: Node) -> void:
 	if body.is_in_group('Actor'):
 		body.surface = fs_name
 
