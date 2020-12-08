6e7b3f9c23f07100ed19616fa2f5566e976e9745
carenalga <mapedorr@gmail.com>
2020-12-08 13:56:48 -0500

    Minor changes in Chapter 0.1 map
    Moved tank and lake masks so they match the position of the tank and the lake.
    Hidden the tank collisions.
    Added sacred fire and temporary pickable to altar's location.

--- game/src/Characters/Demon/States/Idle.gd
+++ game/src/Characters/Demon/States/Idle.gd
@@ -4,6 +4,7 @@ func enter(msg: Dictionary = {}) -> void:
 	.enter()
 
 func _process(delta):
-	_parent.get_node("FeedArea/CollisionShape2D/Feed").scale = sprite.scale
+	pass
+#	_parent.get_node("FeedArea/CollisionShape2D/Feed").scale = scale
 #	$Idle.set_pitch_scale(range_lerp(health, 0, max_health, 3, 1))
 #	$InteractionArea/Area.shape.radius = lerp(1, 20, sprite.scale.y)
