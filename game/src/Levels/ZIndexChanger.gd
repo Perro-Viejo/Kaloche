d4416b9a95212f881045443e05e125681c00668e
carenalga <mapedorr@gmail.com>
2021-01-07 13:54:42 -0500

    Collisions and z-index changers for Altar zone
    The stone and the front columns now have collisions and ZIndexChanger.
    Created ZIndexChanger script for allow Area2D nodes to change the
    z-index of their parent when the Player enters or goes out them.
    < Progress on card $1ii >

---
+++ game/src/Levels/ZIndexChanger.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos de Godot ▒▒▒▒
+func _ready():
+	connect('area_entered', self, '_change_z_index', [true])
+	connect('area_exited', self, '_change_z_index', [false])
+
+
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos privados ▒▒▒▒
+func _change_z_index(body: Area2D, entered: bool) -> void:
+	if body.name == 'FootArea':
+		get_parent().z_index = 2 if entered else 3
