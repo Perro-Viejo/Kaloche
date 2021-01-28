ac0539a42008ebec55e8e314a310edc1384be3d7
carenalga <mapedorr@gmail.com>
2021-01-28 17:41:43 -0500

    Collisions and ZIndex changers for open rod temple
    Some collisions and zindex changers are "disabled" during the emerging
    animation.
    Updated the behavior of some masks to show the tank's water and the main
    platform.
    --- Working on updating the water external collisions ---
    < Progress on $1ok card >

--- game/src/Levels/Chapter01/Zones/RodTemple/RodTemple.gd
+++ game/src/Levels/Chapter01/Zones/RodTemple/RodTemple.gd
@@ -2,18 +2,22 @@ extends Node2D
 
 onready var _animation: AnimationPlayer = $AnimationPlayer
 
+
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos de Godot ▒▒▒▒
 func _ready() -> void:
-	($Temple/ZIndexChanger as ZIndexChanger).connect(
+	($Main/ZIndexChanger as ZIndexChanger).connect(
 		'z_index_changed',
 		self,
 		'_change_temple_mask_range'
 	)
 
 
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos públicos ▒▒▒▒
 func emerge():
 	_animation.play('Emerge')
 
 
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos privados ▒▒▒▒
 func _change_temple_mask_range(new_z_index: int) -> void:
-	$Temple/Mask.range_z_min = new_z_index
-	$Temple/Mask.range_z_max = new_z_index
+	$MainClosedMask.range_z_min = new_z_index
+	$MainClosedMask.range_z_max = new_z_index
