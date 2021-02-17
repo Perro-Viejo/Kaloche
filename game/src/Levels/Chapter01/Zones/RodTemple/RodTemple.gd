8c9da5aae9c393b1f35896d2bff32bf775b48f3f
QuietGeckoAudio <quietgecko@gmail.com>
2021-02-17 10:11:49 -0500

    updated rod temple sounds

--- game/src/Levels/Chapter01/Zones/RodTemple/RodTemple.gd
+++ game/src/Levels/Chapter01/Zones/RodTemple/RodTemple.gd
@@ -10,12 +10,13 @@ func _ready() -> void:
 		self,
 		'_change_temple_mask_range'
 	)
-	_animation.connect('animation_finished', self, '_on_animation_finished')
 
 
 # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ métodos públicos ▒▒▒▒
 func emerge():
 	_animation.play('Emerge')
+	yield(get_tree().create_timer(_animation.get_animation('Emerge').length - 0.2), 'timeout')
+	AudioEvent.emit_signal('play_requested', 'Spot', 'RodTemple', global_position)
 
 
 func get_emerge_animation_length() -> float:
@@ -27,6 +28,3 @@ func _change_temple_mask_range(new_z_index: int) -> void:
 	$MainClosedMask.range_z_min = new_z_index
 	$MainClosedMask.range_z_max = new_z_index
 
-func _on_animation_finished(anim) -> void:
-	if anim == 'Emerge':
-		AudioEvent.emit_signal('play_requested', 'Spot', 'RodTemple', global_position)
