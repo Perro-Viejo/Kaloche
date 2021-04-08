1d4613634386301cdd67387a1ef9a6dc382d3989
carenalga <mapedorr@gmail.com>
2021-04-07 21:53:06 -0500

    Added functionality to teletransport Teotriste
    If the game is running in the Editor, the pause menu can show a list of
    positions to transport Teotriste to.
    The list get filled by the data sent in the WorldEvent[world_entered]
    signal when a level is loaded.

--- game/src/Autoload/Events/WorldEvent.gd
+++ game/src/Autoload/Events/WorldEvent.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal zone_entered(name)
-signal world_entered
+signal world_entered(data)
 signal rod_selected(rod)
 signal pickable_burnt(pickable)
+signal zone_position_requested(position)
--- game/src/GUI/PauseLayer/PauseLayer.gd
+++ game/src/GUI/PauseLayer/PauseLayer.gd
@@ -2,46 +2,64 @@ extends CanvasLayer
 
 export (String, FILE, "*.tscn") var Main_Menu: String
 
+var _world_positions := []
+var _options_list: Resource = preload('res://src/GUI/Buttons/OptionsList.tscn')
+
+onready var _options_container: Container = find_node('OptionsContainer')
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ métodos de Godot ░░░░
 func _ready()->void:
 	GuiEvent.connect("Paused", self, "on_show_paused")
 	GuiEvent.connect("Options", self, "on_show_options")
 	SectionEvent.Paused = false
 	#localization
-	Settings.connect("ReTranslate", self, "retranslate")
+	Settings.connect("ReTranslate", self, "_retranslate")
+	WorldEvent.connect('world_entered', self, '_on_world_entered')
 
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ métodos públicos ░░░░
 func on_show_paused(value:bool)->void:
 	#Signals allow each module have it's own response logic
 	$Control.visible = value
 	get_tree().paused = value
+
 
 func on_show_options(value:bool)->void:
 	AudioEvent.emit_signal('play_requested', 'UI', 'Gen_Button')
 	if !SectionEvent.MainMenu:
 		$Control.visible = !value
 
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ métodos privados ░░░░
 func _on_Resume_pressed():
 	AudioEvent.emit_signal('play_requested', 'UI', 'Gen_Button')
 	SectionEvent.Paused = false #setget triggers signal and responding to it hide GUI
+
 
 func _on_Restart_pressed():
 	GuiEvent.emit_signal('play_requested', 'UI', 'Gen_Button')
 	GuiEvent.emit_signal("Restart")
 	SectionEvent.Paused = false #setget triggers signal and responding to it hide GUI
 
+
 func _on_Options_pressed():
 	AudioEvent.emit_signal('play_requested', 'UI', 'Gen_Button')
 	SectionEvent.Options = true
+
 
 func _on_MainMenu_pressed():
 	AudioEvent.emit_signal('play_requested', 'UI', 'Gen_Button')
 	GuiEvent.emit_signal("ChangeScene", Main_Menu)
 	SectionEvent.Paused = false
 
+
 func _on_Exit_pressed():
 	AudioEvent.emit_signal('play_requested', 'UI', 'Gen_Button')
 	GuiEvent.emit_signal("Exit")
 
-func retranslate()->void:
+
+func _retranslate()->void:
 	find_node("Resume").text = tr("RESUME")
 	find_node("Restart").text = tr("RESTART")
 	find_node("Options").text = tr("OPTIONS")
@@ -49,10 +67,18 @@ func retranslate()->void:
 	find_node("Exit").text = tr("EXIT")
 
 
+func _on_world_entered(data: Dictionary):
+	if OS.has_feature('debug'):
+		if data.has('points'):
+			var pepe: OptionButton = _options_list.instance()
+			pepe.connect('item_selected', self, '_position_selected')
+
+			for p in data.points:
+				_world_positions.append((p as Position2D).get_instance_id())
+				pepe.add_item(p.name, p.get_index())
+
+			_options_container.add_child(pepe)
 
 
-
-
-
-
-
+func _position_selected(index: int) -> void:
+	WorldEvent.emit_signal('zone_position_requested', _world_positions[index])
